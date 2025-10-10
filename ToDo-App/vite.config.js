7941cfc1ed1aadf00e65102881249d6157571584
Ashok Bhaargaw <ashokbhaargaw@gmail.com>
2025-10-10 21:22:39 +0530

    a

--- ToDo-App/vite.config.js
+++ ToDo-App/vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Learning-Projects/ToDo-App/dist/',
   plugins: [tailwindcss(), react()],
 })
