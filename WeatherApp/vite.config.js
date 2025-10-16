2528cb0c4b46c2366d366dc7f518af1a230b5dac
Ashok Bhaargaw <ashokbhaargaw@gmail.com>
2025-10-16 14:38:57 +0530

    App will be life

--- WeatherApp/vite.config.js
+++ WeatherApp/vite.config.js
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/React-Learning-Projects/WeatherApp/dist',
-  plugins: [
-    tailwindcss(),
-    react()],
+  plugins: [tailwindcss(), react()],
 })
