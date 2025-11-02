be0f67014e68b99b9ef003cf123e1622c93c96bd
Ashok Bhaargaw <ashokbhaargaw@gmail.com>
2025-11-02 14:21:24 +0530

    all good

--- BrowserHomepage/src/Components/Clock.tsx
+++ BrowserHomepage/src/Components/Clock.tsx
@@ -1,25 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export default function Clock() {
-  const [date, setdate] = useState(new Date())
+  const [date, setdate] = useState(new Date());
 
   useEffect(() => {
     setInterval(() => {
-      setdate(new Date())
+      setdate(new Date());
     }, 1000);
-  }, [])
+  }, []);
+
+  let hours =
+    date.getHours() <= 12
+      ? date.getHours().toString().padStart(2, "0")
+      : (date.getHours() - 12).toString().padStart(2, "0");
 
   return (
-    <div className=' w-1/2 my-10 flex flex-col '>
-      <h1 className='text-2xl font-bold text-gray-500'>Clock</h1>
-      <div className='flex flex-row items-end'>
-        <h1 className='text-6xl font-bold'>{(date.getHours() <= 12) ? date.getHours().toString().padStart(2, '0') : (date.getHours() - 12).toString().padStart(2, '0')}</h1>
-        <span className='text-5xl font-bold'> : </span>
-        <h1 className='text-6xl font-bold'> {date.getMinutes().toString().padStart(2, '0')}</h1>
-        <span className='text-3xl font-bold'>:</span>
-        <h3 className='text-3xl font-bold text-red-600'>{date.getSeconds().toString().padStart(2, '0')}</h3>
-        <span>{date.getHours() < 12 ? 'am' : 'pm'}</span>
+    <div className=" w-1/2 my-10 flex flex-col ">
+      <h1 className="text-2xl font-bold text-gray-500">Clock</h1>
+      <div className="flex flex-row items-end">
+        <h1 className="text-6xl font-bold">{hours}</h1>
+        <span className="text-5xl font-bold"> : </span>
+        <h1 className="text-6xl font-bold">
+          {" "}
+          {date.getMinutes().toString().padStart(2, "0")}
+        </h1>
+        <span className="text-3xl font-bold">:</span>
+        <h3 className="text-3xl font-bold text-red-600">
+          {date.getSeconds().toString().padStart(2, "0")}
+        </h3>
+        <span>{date.getHours() < 12 ? "am" : "pm"}</span>
       </div>
     </div>
-  )
+  );
 }
